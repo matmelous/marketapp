80c886696b6ee60ad3e4c8b46495339794303d02
teremelomaia <teremelomaia@gmail.com>
2022-08-18 19:31:52 -0300

    wip

--- src/screens/ProductList/ProductList.tsx
+++ src/screens/ProductList/ProductList.tsx
@@ -1,10 +1,22 @@
 import react from "react"
-import {Text} from "react-native"
+import {Text, View, Image} from "react-native"
+import { TextInput } from 'react-native';
+import { Searchbar, } from "react-native-paper";
+import ListAccordionGroup from "react-native-paper/lib/typescript/components/List/ListAccordionGroup";
+import Header from "../../components/Header"
+import {SearchBar } from "./styles";
+import { Entypo } from '@expo/vector-icons';
+
 
 export const ProductList = () => {
   return(
-    <Text>
-      ola
-    </Text>
+
+    <View>
+      <Header/>
+        <SearchBar>
+          <Entypo name="magnifying-glass" size={18} color="black" />
+            <TextInput placeholder="    Buscar produtos"/>
+        </SearchBar>
+    </View>
   )
 }
