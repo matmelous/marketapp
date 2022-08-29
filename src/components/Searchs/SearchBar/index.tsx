0da084052ed95c0207364ba73f66ddde6f4c3907
BrayanSutil <brayansutil81@gmail.com>
2022-08-29 17:54:58 -0300

    wip filter busca

--- src/components/Searchs/SearchBar/index.tsx
+++ src/components/Searchs/SearchBar/index.tsx
@@ -1,13 +1,61 @@
 import { Entypo } from "@expo/vector-icons"
+import { SetStateAction, useState } from "react"
+import { TextInput, Text } from "react-native"
 import { SearchBar, SearchTextInput, Container  } from "./styled"
 
-export const SearchBarProduct = ()=>{
+const list = [
+  {
+    label: 'uno:',
+  },
+  {
+    label: 'gol:',
+  }, {
+    label: 'logan:',
+  }, {
+    label: 'duster:',
+  }, {
+    label: 'opala:',
+  },
+]
+  type Props = {
+    e:{
+      target: {
+        value: SetStateAction<string> 
+      }
+    }
+  }
+
+
+
+export const SearchBarProduct = ({e}: Props) => {
+
+  const [ busca, setBusca ] = useState('')
+
   return(
     <Container>
       <SearchBar>
         <Entypo name="magnifying-glass" size={18} color="black" />
-        <SearchTextInput placeholder="Buscar produtos"/>
+        {/* <SearchTextInput placeholder="Buscar produtos"/> */}
+        <TextInput
+          placeholder="Buscar produtos"
+          value={busca}
+          onChangeText={setBusca}
+        />
+
       </SearchBar>
+          {list.filter((val) => {
+            if(busca === "") {
+              return val
+            } else if (val.label.toLocaleLowerCase().includes(busca.toLocaleLowerCase())) {
+              return val
+            }
+          }).map(list =>{
+          return(
+            <Text key={list.label}>
+              {list.label}
+            </Text>
+          )
+        })}
     </Container>
   )
 }
