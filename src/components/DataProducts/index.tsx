0e700e0e94913e27fe94352ca60564cb0245d50c
João Batista <joaojoaocbi@gmail.com>
2022-10-25 18:18:15 -0300

    Add shipping and confirmation data

--- src/components/DataProducts/index.tsx
+++ src/components/DataProducts/index.tsx
@@ -1,24 +1,43 @@
+import { useState } from "react"
 import {View} from "react-native"
+import { useApi } from "../../hooks/useApi"
 import Header from "../Header"
 import { InputData, ContentInput } from "./styled"
 
-
 export const DataProducts = () => {
+  const api=useApi()
+  const [name, setName ]=useState<string>()
+  const [brand,setBrand]=useState<string>() 
+  const [value,setValue]=useState<string>() 
+  const [weight,seteWeight ]=useState<number>() 
+  const imagens= null
+  const addProduct= async(name: string, brand:string, weight:number, imagens:null)=>{
+  const data= await api.addProducts(name, brand, weight, imagens )
+  console.log(data)
+  }
+  const handleSubmit=async()=>{
+    const data= await api.addProducts(name, brand, weight, imagens )
+  console.log(data)
+  }
   return(
     <View>
       <Header/>
       <ContentInput>
-        <InputData 
+        <InputData  
           placeholder="Nome do Produto"
+          onChangeText={(text)=>setName(text)}
         />
           <InputData 
           placeholder="Marca do Produto"
+          onChangeText={(text)=>setBrand(text)}
         />
           <InputData 
           placeholder="Valor do Produto"
+          onChangeText={(text)=>setValue(text)}
         />
           <InputData 
           placeholder="Peso do Produto"
+          onChangeText={(text)=>seteWeight(text)}
         />
           <InputData 
           placeholder="Nome do Mercado"
