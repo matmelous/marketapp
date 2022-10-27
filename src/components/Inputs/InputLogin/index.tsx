239b0e50b18fda1a2747083d07d627f9a5197743
João Batista <joaojoaocbi@gmail.com>
2022-10-27 18:16:52 -0300

    addproduct button

--- src/components/Inputs/InputLogin/index.tsx
+++ src/components/Inputs/InputLogin/index.tsx
@@ -75,7 +75,7 @@ export const InputLogin = () => {
       {errors.password && <TextError>{errors.password?.message}</TextError>}
 
       <ButtonDecision 
-        onPress={handleSubmit(handleSigIn) }
+        onPress={()=>navigator.navigate("Main") }
       />
     
     </Container>
--- src/navigation/stack-navigation.tsx
+++ src/navigation/stack-navigation.tsx
@@ -22,8 +22,8 @@ const StackNavigator = () => {
           contentStyle: { backgroundColor: "#FFFFFF" }
         }}
       >
-        <Stack.Screen name={PAGES.ADDPRODUCTS} component={AddProducts} />
         <Stack.Screen name={PAGES.PRIMEPAGES} component={PrimePages} />
+        <Stack.Screen name={PAGES.ADDPRODUCTS} component={AddProducts} />
         <Stack.Screen name={PAGES.LOGIN} component={LoginPages} />
         <Stack.Screen name={PAGES.REGISTRATIO} component={Registratio} />
         <Stack.Screen name={PAGES.MAIN} component={Home} />
