624c4ddf1847cc39191e283c0ca0e54a6a38bbec
BrayanSutil <brayansutil81@gmail.com>
2022-12-07 17:06:40 -0300

    tweaks without app

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:'https://yellow-dancers-tap-191-220-97-198.loca.lt',
+  baseURL:'https://swift-onions-rhyme-191-220-97-198.loca.lt',
   responseType: 'json',
 })
